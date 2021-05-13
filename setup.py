20cf87a9e692ced08bd99f3b442bdbdf1b8cd47e
Kevin Hwa <kevin.hwa@polymathian.com>
2021-05-13 22:29:48 +1000

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+requires = [
+    "pulp",
+]
+
+setup(
+    name="sudoku-tasks",
+    version="0.0.0",
+    description="Sudoku GUI Interface + Solver",
+    author="Hawkvine",
+    author_email="liverpool1026.bne@gmail.com",
+    url="https://kevinhwa.com",
+    packages="sudoku_solver",
+    include_package_data=True,
+    install_requires=requires,
+)
