fe7533a4772d7aaffc44ba152fe8665ccebae9f4
Kevin Hwa <kevin.hwa@polymathian.com>
2021-05-16 16:04:02 +1000

    Set up frontend

---
+++ sudoku_solver/static/index.js
@@ -0,0 +1,76 @@
+function generateCellId(index) {
+    return "input_" + index;
+}
+
+function setup() {
+    let grid = this.document.getElementById("sudoku-grid");
+
+    let count = 0;
+    for (let i = 0; i < 9; i++) {
+        let row = this.document.createElement("tr");
+
+        for (let j = 0; j < 9; j++) {
+            let cell = this.document.createElement("td");
+            let input = this.document.createElement("input");
+            input.setAttribute("id", generateCellId(count++));
+            input.setAttribute("type", "number");
+            input.setAttribute("max", 9);
+            input.setAttribute("min", 0);
+            input.setAttribute("step", 1);
+            cell.appendChild(input);
+            row.appendChild(cell);
+        }
+        grid.appendChild(row);
+    }
+
+    let button = this.document.createElement("button");
+    button.innerHTML = "Solve";
+    let clearButton = this.document.createElement("button");
+    clearButton.innerHTML = "Clear";
+
+    button.addEventListener("click", solve);
+    clearButton.addEventListener("click", clear);
+
+    this.document.getElementById("solver").appendChild(button);
+    this.document.getElementById("solver").appendChild(clearButton);
+}
+
+function solve() {
+    let xhttp = new XMLHttpRequest();
+    let that = this;
+
+    let sudokuData = {};
+    let regex = new RegExp("^[0-9]{1}");
+
+    for (let i = 0; i < 81; i++) {
+        cellValue = document.getElementById(generateCellId(i)).value;
+        if (cellValue && cellValue.match(regex)) {
+            sudokuData[i] =  parseInt(cellValue.match(regex)[0]);
+        }
+    }
+
+    xhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            let response = JSON.parse(this.responseText);
+            if (response.status == "success") {
+                Object.keys(response.solution).forEach( function (key) {
+                    console.log(key);
+                    document.getElementById(generateCellId(key)).value = response.solution[key];
+                    document.getElementById(generateCellId(key)).setAttribute("disabled", "disabled");
+                } )
+            }
+        }
+    }
+    xhttp.open("POST", "solve", true);
+    xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    xhttp.send("key=".concat(JSON.stringify(sudokuData)));
+}
+
+function clear() {
+    for (let i = 0; i < 81; i++) {
+        document.getElementById(generateCellId(i)).value = "";
+        document.getElementById(generateCellId(i)).removeAttribute("disabled");
+    }
+}
+
+setup();
