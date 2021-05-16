f251f90d4d3d661978a291305c741f700f30d8be
Kevin Hwa <kevin.hwa@polymathian.com>
2021-05-16 16:32:41 +1000

    Version 1.0.0

--- setup.py
+++ setup.py
@@ -2,17 +2,19 @@ from setuptools import setup
 
 requires = [
     "pulp",
-    "flask",
+    "flask"
 ]
 
 setup(
-    name="sudoku-tasks",
-    version="0.0.0",
+    name="sudoku-solver",
+    version="1.0.0",
     description="Sudoku GUI Interface + Solver",
     author="Hawkvine",
     author_email="liverpool1026.bne@gmail.com",
     url="https://kevinhwa.com",
-    packages="sudoku_solver",
+    packages=["sudoku_solver"],
     include_package_data=True,
     install_requires=requires,
+    entry_points={"console_scripts": ["sudoku=sudoku_solver.run:main"]},
+
 )
